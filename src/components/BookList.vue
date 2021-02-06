4e2c3f90e247959c45e83b9d1bb8b45d140d0df8
Jack <khoicaominh.mmt@gmail.com>
2021-02-06 16:04:57 +0700

    add book list

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="book-list">
+        <h1 class="title">{{ title }}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BookList',
+        data() {
+            return {
+                title: 'All books'
+            };
+        }
+    }
+</script>
+
+<style scoped>
+    .title {
+        font-weight: normal;
+    }
+</style>
